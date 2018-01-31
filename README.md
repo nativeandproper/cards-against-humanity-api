e8a88d61f708008536b0621f367f0136eaa6d320
melindabernrdo <melindabernrdo@gmail.com>
2018-01-31 08:18:36 -0800

    feat: adds signup  user route

--- README.md
+++ README.md
@@ -2,8 +2,50 @@
 Public API for Cards Against Humanity Game
 
 ## API
-To run: `go run main.go` 
+
+## Development 
+
+From project directory: 
+```BASH
+  source .env
+ `go run *.go` 
+ ```
 
 ## Migrations 
 Goose doesn't support running migrations with a Golang binary from a config file yet (https://github.com/pressly/goose/pull/68). So, you need to pass in the config info via the CLI. To Run Goose Migrations from ${GOLANG_PATH}/src/cards-against-humanity/db directory:
-`go run cmd/main.go postgres "user={DB_USER_NAME} dbname={DB_NAME} sslmode=disable" up `
+
+```BASH 
+go run cmd/main.go postgres "user={DB_USER_NAME} dbname={DB_NAME} sslmode=disable" up
+```
+
+## HTTP API Reference
+
+### POST `/v1/signup`
+
+Creates a new user.
+
+#### Request
+
+```json
+{
+  "email": "nativeandproper@gmail.com",
+  "first_name": "Native",
+  "last_name": "Proper",
+  "password": "pass123"
+}
+```
+
+#### Response
+
+```json
+{
+  "success": "ok"
+}
+```
+
+| Status Code | Description                |
+| ----------- | -------------------------- |
+| 201         | Created                    |
+| 400         | Missing or invalid data    |
+| 409         | User already exists        |
+| 503         | Database not available     |
--- server/user_handlers.go
+++ server/user_handlers.go
@@ -4,15 +4,45 @@ import (
 	users "cards-against-humanity-api/users"
 	"encoding/json"
 	"github.com/julienschmidt/httprouter"
+	"golang.org/x/crypto/bcrypt"
 	"net/http"
 )
 
-// signupHandler handles requests to the /signup endpoint
+const hashCost = 14
+
+// HashPassword hashes a password with salt
+func HashPassword(password string) ([]byte, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), hashCost)
+	return bytes, err
+}
+
+// CheckPasswordHash compares a hashed password with a plain-text password
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+// postSignupHandler handles requests to the /signup endpoint
 func (s *Server) postSignupHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	user := &users.User{}
 
-	// decode JSON payload
+	// Decode JSON payload
 	if err := json.NewDecoder(r.Body).Decode(user); err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	// Hash user password
+	hash, err := HashPassword(user.Password)
+	if err != nil {
+		s.Logger.Error().Err(err).Msg("Could not hash password")
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	// Insert new user
+	err = s.User.CreateUser(user, hash)
+	if err != nil {
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
