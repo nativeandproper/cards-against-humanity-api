b35eb43e7f64f195302f4487ee970cb3434ec684
melindabernrdo <melindabernrdo@gmail.com>
2018-10-13 23:33:39 -0700

    updates variable naming in rate limiter Enforce method

--- ratelimiter/limiter.go
+++ ratelimiter/limiter.go
@@ -2,9 +2,9 @@ package ratelimiter
 
 import (
 	"errors"
-	"fmt"
 	"github.com/go-redis/redis"
 	"math"
+	"strconv"
 	"time"
 )
 
@@ -27,43 +27,53 @@ func New(redisClient *redis.Client, options *LimiterOptions) *Limiter {
 	}
 }
 
-// Enforce enforces rate limit
+// Enforce enforces the rate limit and remaining requests
 func (l *Limiter) Enforce(key string) (int64, error) {
 	if l.Limit == 0 {
 		return 0, errors.New("no limit set")
 	}
 
-	prefixedKey := l.prefixKey(key)
+	pKey := l.prefixKey(key)
+	now := time.Now().Unix()
 
-	now := time.Now()
-	prevWindow := now.Add(-l.SlidingWindow)
+	minScore := stringifyInt64AsFloat(0)
+	prevMaxScore := stringifyInt64AsFloat(time.Now().Add(-l.SlidingWindow).Unix())
+	currMaxScore := stringifyInt64AsFloat(now)
+
 	member := redis.Z{
-		Score:  float64(now.Unix()),
-		Member: float64(now.Unix()),
+		Score:  float64(now),
+		Member: float64(now),
 	}
 
+	rangeBy := redis.ZRangeBy{
+		Min: minScore,
+		Max: prevMaxScore,
+	}
+
+	// Execute transaction
 	pipe := l.client.TxPipeline()
-	pipe.ZRangeByScoreWithScores(prefixedKey, redis.ZRangeBy{
-		Min: fmt.Sprintf("%f", float64(0)),
-		Max: fmt.Sprintf("%f", float64(prevWindow.Unix())),
-	})
-	// TODO: check count before incrementing in LUA script
-	pipe.ZAdd(prefixedKey, member)
-	pipe.ZCount(prefixedKey, fmt.Sprintf("%f", float64(0)), fmt.Sprintf("%f", float64(now.Unix())))
-	pipe.Expire(prefixedKey, l.SlidingWindow)
+	pipe.ZRangeByScoreWithScores(pKey, rangeBy)
+	pipe.ZAdd(pKey, member)
+	pipe.ZCount(pKey, minScore, currMaxScore)
+	pipe.Expire(pKey, l.SlidingWindow)
 
 	cmds, err := pipe.Exec()
 	if err != nil {
 		return 0, err
 	}
 
-	var remaining int64
+	var remReq int64
 	if len(cmds) >= 2 {
-		reqCount := cmds[2].(*redis.IntCmd).Val()
-		remaining = int64(math.Max(float64(0), float64(l.Limit-reqCount)))
+		zAddResult := cmds[2]
+		totalReq := zAddResult.(*redis.IntCmd).Val()
+		remReq = int64(math.Max(float64(0), float64(l.Limit-totalReq)))
 	}
 
-	return remaining, nil
+	return remReq, nil
+}
+
+func stringifyInt64AsFloat(num int64) string {
+	return strconv.FormatFloat(float64(num), 'f', 0, 64)
 }
 
 func (l *Limiter) prefixKey(key string) string {
