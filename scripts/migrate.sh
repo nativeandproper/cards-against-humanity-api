85c0d0d1002b6d648486aa46b584a06f439633d3
melindabernrdo <melindabernrdo@gmail.com>
2019-01-01 18:57:15 -0800

    updates migration file

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -7,5 +7,5 @@ DB_NAME=cah_dev
 MIGRATION_DIR=db/cmd
 
 echo running migrations on $DB_NAME ...
-go run $MIGRATION_DIR/main.go postgres "user=$DB_USER password=$DB_PASSWORD dbname=$DB_NAME sslmode=disable" status
+go run $MIGRATION_DIR/main.go postgres "user=$DB_USER password=$DB_PASSWORD dbname=$DB_NAME sslmode=disable" up
 echo migrations complete
